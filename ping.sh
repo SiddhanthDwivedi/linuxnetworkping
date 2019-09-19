7d627c475feadb73cba673d734a7d3d6051c85db
Siddhanth Dwivedi <hackersiddhant@hotmail.com>
2019-09-19 23:54:30 +0530

    code added

---
+++ ping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$1" == ""]
+then
+echo "Usage: ./pingh.sh [network]"
+echo "Example: ./pingh.sh 192.168.1"
+else
+for x in seq 1 254 :do
+ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//'
+done
+fi
